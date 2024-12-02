50fd35050d1f1c7b68a1da6145e45c4a3c4c2486
apblanchard4 <107159438+apblanchard4@users.noreply.github.com>
2024-12-02 15:22:54 -0500

    Lint errors fixed on admin

--- src/app/admin/adminDashboard/page.tsx
+++ src/app/admin/adminDashboard/page.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-
 
 function AdminDashboard() {
-    const router = useRouter();
-
 
     return (
         <div className="container mx-auto p-6 bg-gray-100 rounded-lg shadow-lg">
